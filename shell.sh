1b1a1a691ff1cae1ba494f742d83e4924a62ed70
Sandeepu8 <sdeepu1988@gmail.com>
2025-08-11 17:21:50 +0530

    Shell Command

---
+++ shell.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+echo "R u learning Linux and if YES, why?"
+read -s b
+if [ $b = yes ]
+then 
+	echo "Ofcourse"
+echo "Today iam Learning the Linux to complete the course Devops"
+else 
+	echo "Upgrade your Skills, soon buddy!"
+fi
+function hi(){
+echo "Enter your name here...!"
+read -s a
+echo "Enter your Password"
+read -s Password
+if [ $a = sandeep ] && [ $Password = 560116 ]
+then 
+echo " Thank you for your confirmation, you entered name is $a."
+date
+function hello() {
+	for((i=1;i<=5;i++))
+		do
+			echo "function running..."
+		done
+	}
+	hello
+	echo "Successfully created"
+awk 'NR==18 {print $3, $4}' shell.sh
+pwd
+else 
+	echo "Please enter Correct Name and Password and Try again... Thankyou for vist."
+fi
+}
+hi
+
